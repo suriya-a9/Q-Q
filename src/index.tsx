c7989146ecef70604120b64256160c2208857860
suriya-a9 <110898451+suriya-a9@users.noreply.github.com>
2024-06-25 11:55:33 +0530

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Home from './views/Home/Home';
 import Services from './views/Services/Services';
 import Appointment from './views/Booking/Appointment';
