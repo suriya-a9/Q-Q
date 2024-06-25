913a15544445d2682f021e4f4627975d7a6e9475
Suriya prakash <uriyaprakash@gmail.com>
2024-06-25 12:03:54 +0530

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { HashRouter, Route, Routes } from "react-router-dom";
-import Home from './views/Home/Home';
-import Services from './views/Services/Services';
-import Appointment from './views/Booking/Appointment';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./views/Home/Home";
+import Services from "./views/Services/Services";
+import Appointment from "./views/Booking/Appointment";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
-  <HashRouter>
-      <Routes>
-        {/* <App /> */}
-        <Route path="/welcome-page" element={<App />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/book-an-appointment" element={<Appointment />} />
-        <Route path="/services" element={<Services />} />
-      </Routes>
-  </HashRouter>
+  <BrowserRouter>
+    <Routes>
+      {/* <App /> */}
+      <Route path="/welcome-page" element={<App />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/book-an-appointment" element={<Appointment />} />
+      <Route path="/services" element={<Services />} />
+    </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
